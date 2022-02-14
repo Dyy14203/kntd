dea0768bd76c2b3b11ef31fcdca2411acfd09db7
lynnnnzx <rifqikanekii140202@gmail.com>
2022-02-14 19:17:03 +0700

    weeb
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

---
+++ final.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export BUILD_USERNAME=lynx
+export BUILD_HOSTNAME=cirrus-ci
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 100G -F 0
+ccache -o compression=true
+ccache -z
+
+$lunch
+$make -j8
+
